abc5652e965bb01490cbc5e8fd96e465479313b0
xiaoxin <1370174361@qq.com>
2018-03-27 20:01:51 +0800

    manager的处理

---
+++ Ihome_Project/config.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+class Config(object):
+    DEBUG = True
+    TESTING = False
+    DATABASE_URL = 'mysql://root:mysql@127.0.0.1/ihome06'
+
--- Ihome_Project/manager.py
+++ Ihome_Project/manager.py
@@ -1,8 +1,19 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import MigrateCommand,Migrate
+from flask_script import Manager
 app = Flask(__name__)
 
+#  配置,迁移数据库
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:mysql@127.0.0.1/ihome06'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+manager = Manager(app)
+db = SQLAlchemy(app)
+
+Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
 
 @app.route('/')
 def hello_world():
@@ -10,4 +21,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
