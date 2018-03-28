35eba2455bcbf6d2c28a4d231ea85bb319a373f9
xiaoxin <1370174361@qq.com>
2018-03-28 10:23:08 +0800

    设置日志文件

--- Ihome_Project/ihome/api_1_0/index.py
+++ Ihome_Project/ihome/api_1_0/index.py
@@ -1,11 +1,16 @@
 # -*- coding:utf-8 -*-
 # 导入蓝图对象
 from flask import session
-
+import logging
 from . import api
 from ihome import db
 
 @api.route('/')
 def hello_world():
     session['name']='xiaobai'
+    logging.debug("This is a debug log.")
+    logging.info("This is a info log.")
+    logging.warning("This is a warning log.")
+    logging.error("This is a error log.")
+    logging.critical("This is a critical log.")
     return 'Hello World!'
